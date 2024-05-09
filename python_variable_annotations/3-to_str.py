d188e64cf86014123b08c1a7f08734e1833cfa5c
Vulgence <e.kinyua@alustudent.com>
2024-05-09 09:39:08 +0300

    to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    return the string representation of a float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Args:
+            n: float number
+
+        Return:
+            returns the string representation of the float.
+    """
+
+    return str(n)

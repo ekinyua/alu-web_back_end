87a60c6665d5c9caea28fb5f619671874a607823
Vulgence <e.kinyua@alustudent.com>
2024-05-09 09:37:01 +0300

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Float to integer truncated using floor division
+"""
+
+
+def floor(n: float) -> int:
+    """
+        Args:
+            n: float number
+
+        Return:
+            Float number truncated to the floor division value of the number
+    """
+
+    return int(n)

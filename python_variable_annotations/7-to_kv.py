f07a1121fa5d8c065eadc499e22752d948bf7269
Vulgence <e.kinyua@alustudent.com>
2024-05-09 10:00:11 +0300

    string and int/float to tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    Mixed Tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+        Args:
+            k: String
+            v: Union: Can either be an int or a float
+
+        Return:
+            Tuple with string as first element and square of int or float as second element, annotated as a float
+    """
+
+    cncat: Tuple(str, Union[int, float])
+    cncat = (k, v**2)
+
+    return cncat

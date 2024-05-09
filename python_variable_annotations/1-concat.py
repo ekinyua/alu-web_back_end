759dddca22584bdf77a30e2bf388b2ea86f69523
Vulgence <e.kinyua@alustudent.com>
2024-05-09 09:33:23 +0300

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    String with annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Args:
+            str1: The first string parameter
+            str1: The second string parameter
+
+        Return:
+            function returns a Concatenated string
+    """
+
+    return (str1 + str2)

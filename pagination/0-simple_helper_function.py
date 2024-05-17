9c66372560d3ac8ab76d383556631ffa34889fea
Vulgence <e.kinyua@alustudent.com>
2024-05-17 11:12:33 +0300

    helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -13,7 +13,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
             tuple with the range start and end size page
     """
 
-    final_size: int = page * page_size
-    start_size: int = final_size - page_size
+    final_index: int = page * page_size
+    start_index: int = final_index - page_size
 
-    return (start_size, final_size)
+    return (start_index, final_index)

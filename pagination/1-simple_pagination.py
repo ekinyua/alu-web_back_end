3d376a0455b03830bed045cd9f1d18a908f036b6
Vulgence <e.kinyua@alustudent.com>
2024-05-17 14:04:12 +0300

    simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""Method named get_page that takes 2 integers"""
+
+import csv
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """server class to paginate database of popular baby names"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(Self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                render = csv.reader(f)
+                dataset = [row for row in render]
+            self.__dataset = dataset[1:]
+     
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Dataset output page"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+  
+        indices = index_range(page, page_size)
+        start = indices[0]
+        end = indices[1]
+
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return[]

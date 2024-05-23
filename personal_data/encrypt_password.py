de8b20cc9c6e23bb38e481c755003d3e4e9e37ac
Vulgence <e.kinyua@alustudent.com>
2024-05-23 23:36:06 +0300

    check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -15,5 +15,9 @@ def hash_password(password):
         bytes: The salted, hashed password.
     """
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if password is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

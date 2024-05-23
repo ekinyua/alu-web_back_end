566990f6d48dad60b401af4e52a9f69f8b2fadcc
Vulgence <e.kinyua@alustudent.com>
2024-05-23 23:04:04 +0300

    log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -25,14 +25,13 @@ class RedactingFormatter(logging.Formatter):
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-    
+
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-    
+
     def format(self, record: logging.LogRecord) -> str:
         """ using filter_datum to filter values in incoming log records"""
-        record.mg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-    
-        return super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)

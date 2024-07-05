62852089dedc8bc2e5c8213061e5b8190b298dc6
Vulgence <e.kinyua@alustudent.com>
2024-07-06 01:56:12 +0300

    logs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,29 +2,28 @@
 """python scripts"""
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+def log_stats(mongo_collection):
+    """Script that provides some stats about Nginx logs stored in MongoDB."""
+    # Total number of log entries
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
 
-def log_stats(mongo_collection, option=None):
-    """ script that provides some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+    # Methods count
     print("Methods:")
     for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
+        method_count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
 
+    # Status check count
+    status_check_count = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    # Connect to the MongoDB server and select the nginx collection
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    # Call the log_stats function
     log_stats(nginx_collection)

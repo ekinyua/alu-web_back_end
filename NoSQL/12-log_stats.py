40ee2acd2059053f8040330c8e06e8662846dae7
Vulgence <e.kinyua@alustudent.com>
2024-07-05 23:39:56 +0300

    log

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
-"""
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
-"""
+"""python scripts"""
 from pymongo import MongoClient
+
 
 
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
+    """ script that provides some stats about Nginx logs stored in MongoDB
     """
     items = {}
     if option:
